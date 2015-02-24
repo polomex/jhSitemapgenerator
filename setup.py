8f520624e67df98011b91574c5c80bc18a7a2607
Jan Helbling <jan.helbling@gmail.com>
2015-02-24 12:07:55 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='jhSitemapgenerator',
+      version='0.1.2',
+      description='A multithreaded commandline tool to create sitemap.xml|.gz|.txt files from a website.',
+      license='GPL3+',
+      author='Jan Helbling',
+      author_email='jan.helbling@gmail.com',
+      url='http://www.jan-helbling.ch/~jhelbling/linux.py?gpl3-opensource-programm=jhSitemapgenerator-a-multithreaded-tool-to-create-sitemap.xml-from-shell',
+      platforms=['linux','freebsd','netbsd','unixware7' , 'openbsd' , 'windows'],
+      scripts=['bin/jhSitemapgenerator.py'],
+)
